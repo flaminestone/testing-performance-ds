4f1c2fd0e0c1bf266811f5d0cf6fe0af6e8d0670
Dmitriy Rotatyy <flamine@list.ru>
2019-11-22 17:16:38 +0300

    Separation database functionality outside of front

---
+++ src/public/javascript/database.js
@@ -0,0 +1,16 @@
+class Database {
+    static send_result(data) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/add_result', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    }
+
+    static get_results(username) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/results', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(username));
+    }
+
+}
