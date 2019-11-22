6635ba1d8a9fdf709e0664fd9c7b049af2361a54
Dmitriy Rotatyy <flamine@list.ru>
2019-11-22 17:15:57 +0300

    Separation database functionality outside of all app

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -7,16 +7,23 @@ interface Result {
     time: number
 
 }
+
 export class Database {
     readonly db: any;
     constructor() {
         this.db = low(new FileSync('db.json'));
-        this.db.defaults({ results: []}).write();
+        this.db.defaults({ data: []}).write();
     }
 
     add_result(result: Result) {
-        this.db.get('results')
-            .push(result)
-            .write()
+        if (!this.db.get('data').find({username: result.username, filename: result.filename}).value()) {
+            this.db.get('data').push({username: result.username, filename: result.filename, results: []}).write();
+        }
+        this.db.get('data').find({username: result.username, filename: result.filename}).get('results').push(result.time).write()
+
+    }
+
+    get_results(username) {
+        return this.db.get('data').find({username: username}).values()
     }
 }
