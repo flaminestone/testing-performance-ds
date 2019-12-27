7d0acbc69cbb4a7a6ce0e35847342f6259027d76
Dmitriy Rotatyy <flamine@list.ru>
2019-12-27 13:57:34 +0300

    update plugin document for using more clients

--- src/public/plugin/app.js
+++ src/public/plugin/app.js
@@ -18,21 +18,16 @@ for (let i = 0; i < 100; i) {
         var oDocument = Api.GetDocument();
         var oParagraph, oParaPr;
         var oParaStyle = oDocument.GetDefaultStyle("paragraph");
-        oParagraph = oDocument.GetElement(Math.floor(Math.random() * 141));
+        oParagraph = oDocument.GetElement(Math.floor(Math.random() * 1320));
         oParagraph.SetStyle(oParaStyle);
         oParaPr = oParagraph.GetParaPr();
-        oParaPr.SetJc("both");
-        oParaPr.SetIndFirstLine(720);
-        oParaPr.SetBottomBorder("single", 24, 0, Asc.scope.r, Asc.scope.g, Asc.scope.b);
         oParaPr.SetBetweenBorder("single", 24, 0, Asc.scope.r, Asc.scope.g, Asc.scope.b);
-        oParaPr.SetLeftBorder("single", 24, 0, Asc.scope.r, Asc.scope.g, Asc.scope.b);
-        oParaPr.SetTopBorder("single", 24, 0, Asc.scope.r, Asc.scope.g, Asc.scope.b);
       }, false, '',  function () {
           let xhr = new XMLHttpRequest();
           xhr.open("POST", 'http://178.128.186.166:8000/open/activity', true);
           xhr.setRequestHeader('Content-Type', 'application/json');
           xhr.send(JSON.stringify({ username: window.Asc.plugin.info.userName}));
-        setTimeout(() => resolve(), 200);
+        setTimeout(() => resolve(), 1000);
       });
     })
   }
